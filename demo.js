5280fe4977150654ef87d30712a975b188037d47
senaru <s3696653@student.rmit.edu.au>
2020-07-12 14:25:18 +1000

    Writing to a file

--- demo.js
+++ demo.js
@@ -1,15 +1,28 @@
 var _ = require('lodash');
 var fs = require('fs');
-var data = require('./data.json');
 
+//Rand
+console.log(_.random(1, 100));
+
+//Reading a json
+var data = require('./data.json');
 console.log(data.name)
 
+//Read File
 fs.readFile('./data.json', 'UTF-8', (err, data) => {
     console.log(data)
 })
 
+//Read Dir
 fs.readdir('c:/', (err, data) => {
     console.log(data)
 })
 
-console.log(_.random(1, 100));
+var data = {
+    name: "bob"
+}
+
+//Writing to a file
+fs.writeFile('data2.json', JSON.stringify(data), (err) => {
+    if (err) console.log('error', err);
+});
