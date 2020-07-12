ac76a176b7c5bed2ae41f3a5d6a1c7d656241149
senaru <s3696653@student.rmit.edu.au>
2020-07-12 14:41:04 +1000

    serving a static site with express

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname))
+
+const server = app.listen(3000, () => {
+    console.log("server is listening on port", server.address().port);
+})
