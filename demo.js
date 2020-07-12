0e8636241a9823ce1910f9717335a2343e274521
senaru <s3696653@student.rmit.edu.au>
2020-07-12 14:08:37 +1000

    reading files

--- demo.js
+++ demo.js
@@ -1,3 +1,11 @@
 var _ = require('lodash');
+var fs = require('fs');
+var data = require('./data.json');
 
-console.log(_.random(1, 10));
+console.log(data)
+
+fs.readFile('./data.json', 'UTF-8', (err, data) => {
+    console.log(JSON.parse(data).name)
+})
+
+console.log(_.random(1, 100));
