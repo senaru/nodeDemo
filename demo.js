3335e71f7b6cfd7a77b5fc31ca869ed994f23251
senaru <s3696653@student.rmit.edu.au>
2020-07-12 14:10:33 +1000

    reading directories

--- demo.js
+++ demo.js
@@ -2,10 +2,14 @@ var _ = require('lodash');
 var fs = require('fs');
 var data = require('./data.json');
 
-console.log(data)
+console.log(data.name)
 
 fs.readFile('./data.json', 'UTF-8', (err, data) => {
-    console.log(JSON.parse(data).name)
+    console.log(data)
+})
+
+fs.readdir('c:/', (err, data) => {
+    console.log(data)
 })
 
 console.log(_.random(1, 100));
