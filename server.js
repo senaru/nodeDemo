78ac5090ae09cbfd1d08af3f43ad70ec7c975db2
senaru <s3696653@student.rmit.edu.au>
2020-07-18 12:36:41 +1000

    Creating a get message service

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var express = require('express')
+var bp = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bp.json())
+
+var messages = [
+    { name: "Tim", message: "Hi" },
+    { name: "Owin", message: "Hello!" }
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body)
+    console.log(req.body);
+    res.sendStatus(200);
+})
 
 const server = app.listen(3000, () => {
     console.log("server is listening on port", server.address().port);
